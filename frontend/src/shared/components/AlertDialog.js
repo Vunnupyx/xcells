56bdf845aac83608fdfc08ebf6724967f9296b1b
Vunnupyx <muhutdinovrifat@yandex.ru>
2023-05-10 16:01:51 +0400

    fix: fix test case, linting and formatting

--- frontend/src/shared/components/AlertDialog.js
+++ frontend/src/shared/components/AlertDialog.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import {DialogContentText, Dialog, DialogActions, DialogContent, DialogTitle} from '@material-ui/core'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
 import Button from '@material-ui/core/Button'
 import {FormattedMessage, useIntl} from 'react-intl'
 
